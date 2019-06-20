7e710ef1e9881fda8d7d84b4b7254be198591f56
rocky-191 <1375022007@qq.com>
2019-06-20 22:47:36 +0800

    add redux-thunk，解决异步获取数据，数据使用了模拟数据

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {connect} from 'react-redux'
 import { CSSTransition } from 'react-transition-group';
 
@@ -20,28 +20,34 @@ import {
     SearchInfoList
 } from './style';
 
-const getListArea = (show)=>{
-    if(show){
-        return (
-            <SearchInfo>
-                <SearchInfoTitle>
-                    热门搜索
-                    <SearchInfoSwitch>换一批</SearchInfoSwitch>
-                </SearchInfoTitle>
-                <SearchInfoList>
-                    <SearchInfoItem>教育</SearchInfoItem>
-                    <SearchInfoItem>商业</SearchInfoItem>
-                </SearchInfoList>
-            </SearchInfo>
-        )
-    }else{
-        return null;
-    }
-}
+class Header extends Component {
 
-const Header =(props)=>{
-    return (
-        <HeaderWrapper>
+    getListArea = ()=>{
+        if(this.props.focused){
+            return (
+                <SearchInfo>
+                    <SearchInfoTitle>
+                        热门搜索
+                        <SearchInfoSwitch>换一批</SearchInfoSwitch>
+                    </SearchInfoTitle>
+                    <SearchInfoList>
+                        {
+                            this.props.list.map(item=>{
+                                return (
+                                    <SearchInfoItem for={item}>{item}</SearchInfoItem>
+                                )
+                            })
+                        }
+                    </SearchInfoList>
+                </SearchInfo>
+            )
+        }else{
+            return null;
+        }
+    }
+    render(){
+        return (
+            <HeaderWrapper>
             <Logo href="/" />
             <Nav>
                 <NavItem className="left active">首页</NavItem>
@@ -52,14 +58,14 @@ const Header =(props)=>{
                 </NavItem>
                 <SearchWrapper>
                     <CSSTransition
-                        in={props.focused}
+                        in={this.props.focused}
                         timeout={200}
                         classNames="slide"
                     >
-                        <NavSearch className={props.focused ? 'focused' : ''} onFocus={props.handleInputFocus} onBlur={props.handleInputBlur}></NavSearch>
+                        <NavSearch className={this.props.focused ? 'focused' : ''} onFocus={this.props.handleInputFocus} onBlur={this.props.handleInputBlur}></NavSearch>
                     </CSSTransition>
-                    <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
-                    {getListArea(props.focused)}
+                    <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
+                    {this.getListArea()}
                 </SearchWrapper>
                 <Addition>
                     <Button className="writting">
@@ -70,18 +76,74 @@ const Header =(props)=>{
                 </Addition>
             </Nav>
         </HeaderWrapper>
-    )
+        )
+    }
 }
+
+// const getListArea = (show)=>{
+//     if(show){
+//         return (
+//             <SearchInfo>
+//                 <SearchInfoTitle>
+//                     热门搜索
+//                     <SearchInfoSwitch>换一批</SearchInfoSwitch>
+//                 </SearchInfoTitle>
+//                 <SearchInfoList>
+//                     <SearchInfoItem>教育</SearchInfoItem>
+//                     <SearchInfoItem>商业</SearchInfoItem>
+//                 </SearchInfoList>
+//             </SearchInfo>
+//         )
+//     }else{
+//         return null;
+//     }
+// }
+
+// const Header =(props)=>{
+//     return (
+//         <HeaderWrapper>
+//             <Logo href="/" />
+//             <Nav>
+//                 <NavItem className="left active">首页</NavItem>
+//                 <NavItem className="left">下载App</NavItem>
+//                 <NavItem className="right">登陆</NavItem>
+//                 <NavItem className="right">
+//                     <i className="iconfont">&#xe636;</i>
+//                 </NavItem>
+//                 <SearchWrapper>
+//                     <CSSTransition
+//                         in={props.focused}
+//                         timeout={200}
+//                         classNames="slide"
+//                     >
+//                         <NavSearch className={props.focused ? 'focused' : ''} onFocus={props.handleInputFocus} onBlur={props.handleInputBlur}></NavSearch>
+//                     </CSSTransition>
+//                     <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
+//                     {getListArea(props.focused)}
+//                 </SearchWrapper>
+//                 <Addition>
+//                     <Button className="writting">
+//                         <i className="iconfont">&#xe615;</i>
+//                         写文章
+//                     </Button>
+//                     <Button className="reg">注册</Button>
+//                 </Addition>
+//             </Nav>
+//         </HeaderWrapper>
+//     )
+// }
 
 const mapStateToProps = (state, ownProps) => {
     return {
-        focused:state.get('header').get('focused')
+        focused:state.get('header').get('focused'),//两种写法等效
+        list:state.getIn(['header','list'])
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
         handleInputFocus(){
+            dispatch(actionCreators.getList())
             dispatch(actionCreators.searchFocus())
         },
         handleInputBlur(){
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
-import {createStore,compose} from 'redux';
+import {createStore,compose,applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 import reducers from './reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store=createStore(
     reducers,
-    composeEnhancers()
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
