3ab66a82aff139c85cd971e057fc2114b1545f46
rocky-191 <1375022007@qq.com>
2019-06-30 21:34:22 +0800

    详情页

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,5 +1,11 @@
 import React, { PureComponent } from 'react';
 
+import {
+    DetailWrapper,
+    DetailTitle,
+    Content
+} from './style';
+
 class Detail extends PureComponent {
     constructor(props) {
         super(props);
@@ -7,9 +13,10 @@ class Detail extends PureComponent {
     }
     render() {
         return (
-            <div>
-                detail
-            </div>
+            <DetailWrapper>
+                <DetailTitle>学了这几个CMD指令，以后你就是别人眼中的大神</DetailTitle>
+                <Content>熟悉电脑的小伙伴应该会知道有着不少的CMD指令可以让我们在平时更加快捷方便的去对电脑做一些数据上的更改，比如利用CMD指令查找注册表，然后修改一个小小的数据，就可以让你的电脑有一些本不能运行的软件正常运行。今天我们就来教大家一些别人眼中的逼格满满的“大神”级别的操作指令。</Content>
+            </DetailWrapper>
         );
     }
 }
---
+++ src/pages/detail/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const DetailWrapper=styled.div`
+    padding-bottom: 40px;
+    width: 620px;
+    margin:0 auto;
+    overflow:hidden;
+`;
+
+export const DetailTitle=styled.div`
+    word-break: break-word!important;
+    word-break: break-all;
+    margin: 20px 0 0;
+    font-family: -apple-system,SF UI Display,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,sans-serif;
+    font-size: 34px;
+    font-weight: 700;
+    line-height: 44px;
+`;
+
+export const Content=styled.div`
+    color: #2f2f2f;
+    word-break: break-word!important;
+    word-break: break-all;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.7;
+`;
